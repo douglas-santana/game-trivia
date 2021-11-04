f750e4cdcadc8a279cff248b38f246282dd14d97
Douglas-Santana <douguinhodns@gmail.com>
2021-11-04 20:11:44 -0300

    Updates

---
+++ precache-manifest.c7ec0b9b9e684033e17f2a44642f25d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fdb618ae6a6dbec74a0566a56dcd7b7",
+    "url": "/game-trivia/index.html"
+  },
+  {
+    "revision": "b97dd64fdf55cc67597d",
+    "url": "/game-trivia/static/css/main.05186763.chunk.css"
+  },
+  {
+    "revision": "6ff448f77f181952a4f4",
+    "url": "/game-trivia/static/js/2.e05d8293.chunk.js"
+  },
+  {
+    "revision": "5d1b8067085602e259fd0862f57df793",
+    "url": "/game-trivia/static/js/2.e05d8293.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b97dd64fdf55cc67597d",
+    "url": "/game-trivia/static/js/main.af71acb5.chunk.js"
+  },
+  {
+    "revision": "dbeba09c95bfe69c40c3",
+    "url": "/game-trivia/static/js/runtime-main.cc8752fd.js"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/game-trivia/static/media/trivia.466d153e.png"
+  }
+]);
