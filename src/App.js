d1b316dd453aaeebdf24bbe6e7462ce710326a11
Douglas-Santana <douguinhodns@gmail.com>
2021-11-04 20:09:39 -0300

    chenged path/ to path/game-trivia

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Feedback from './pages/Feedback';
 export default function App() {
   return (
     <Switch>
-      <Route exact path="/" component={ Login } />
+      <Route exact path="/game-trivia" component={ Login } />
       <Route path="/settings" component={ Config } />
       <Route path="/jogo" component={ Game } />
       <Route path="/ranking" component={ Ranking } />
